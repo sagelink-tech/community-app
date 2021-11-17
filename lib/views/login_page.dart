66a6f9793f925864ed1038f373973f160494af9f
Evan Lee <evantlee@gmail.com>
2021-11-17 15:55:45 -0500

    Second test user

--- lib/views/login_page.dart
+++ lib/views/login_page.dart
@@ -5,18 +5,27 @@ import '../providers.dart';
 class LoginPage extends ConsumerWidget {
   const LoginPage({Key? key}) : super(key: key);
 
-  final userId = "30172a8c-c407-4852-b5b4-d0dedb39bde9";
-
+  final userId1 = "30172a8c-c407-4852-b5b4-d0dedb39bde9";
+  final userId2 = "cd329013-5cb6-4bea-882a-8b7a4591dd11";
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final loggedInUser = ref.watch(loggedInUserProvider.notifier);
 
     return Scaffold(
       body: Center(
-        child: TextButton(
-          child: const Text("Login"),
-          onPressed: () => {loggedInUser.loginWithUserId(userId)},
-        ),
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              TextButton(
+                child: const Text("Login 1"),
+                onPressed: () => {loggedInUser.loginWithUserId(userId1)},
+              ),
+              TextButton(
+                child: const Text("Login 2"),
+                onPressed: () => {loggedInUser.loginWithUserId(userId2)},
+              ),
+            ]),
       ),
     );
   }
