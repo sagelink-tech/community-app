dc58005462567dce084672f40e6bbb86ca69fa8a
Evan Lee <evantlee@gmail.com>
2022-01-09 16:39:56 -0500

    Remove tuple

--- lib/models/logged_in_user.dart
+++ lib/models/logged_in_user.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:sagelink_communities/models/user_model.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
-import 'package:tuple/tuple.dart';
 
 String getUserQuery = """
 query Users(\$where: UserWhere) {
