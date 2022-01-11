69db705bb663f0f64bdd4f23a0f58f7f57a706eb
Evan Lee <evantlee@gmail.com>
2022-01-11 17:27:23 -0500

    Scaffold with splitview

---
+++ lib/components/page_scaffold.dart
@@ -0,0 +1,45 @@
+// Based on the code found at: https://codewithandrea.com/articles/flutter-responsive-layouts-split-view-drawer-navigation/
+import 'package:flutter/material.dart';
+
+class PageScaffold extends StatelessWidget {
+  const PageScaffold({
+    Key? key,
+    required this.title,
+    this.actions = const [],
+    this.body,
+    this.floatingActionButton,
+    this.appBarElevation,
+  }) : super(key: key);
+  final String title;
+  final List<Widget> actions;
+  final Widget? body;
+  final Widget? floatingActionButton;
+  final double? appBarElevation;
+
+  @override
+  Widget build(BuildContext context) {
+    // 1. look for an ancestor Scaffold
+    final ancestorScaffold = Scaffold.maybeOf(context);
+    // 2. check if it has a drawer
+    final hasDrawer = ancestorScaffold != null && ancestorScaffold.hasDrawer;
+    return Scaffold(
+      appBar: AppBar(
+        // 3. add a non-null leading argument if we have a drawer
+        leading: hasDrawer
+            ? IconButton(
+                icon: const Icon(Icons.menu),
+                // 4. open the drawer if we have one
+                onPressed:
+                    hasDrawer ? () => ancestorScaffold!.openDrawer() : null,
+              )
+            : null,
+        title: Text(title),
+        actions: actions,
+        elevation: appBarElevation,
+        backgroundColor: Theme.of(context).backgroundColor,
+      ),
+      body: body,
+      floatingActionButton: floatingActionButton,
+    );
+  }
+}
